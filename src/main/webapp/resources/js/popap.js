a493a09c3981ac63c92a91122de7ddd6dfe4ecd8
zumek1 <2Tupac148>
2019-07-16 10:22:30 +0200

    zablokowano mozliwosc wpisania wartosci ujemnej do zamwowienia przez PM

--- src/main/webapp/resources/js/popap.js
+++ src/main/webapp/resources/js/popap.js
@@ -14,6 +14,17 @@ $(document).ready(function () {
         }
     })
 
+    var inputy = $('tr').find('input')
+    inputy.value==null
+    inputy.on('keydown', function (e) {
+        if(!((e.keyCode>95 && e.keyCode<106)
+        || (e.keyCode>47 && e.keyCode<58)
+        ||e.keyCode==8)){
+            return false;
+        }
+
+    })
+
 
 
 
